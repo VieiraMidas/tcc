2b91fe42b7d37c6057d82560258227c1c269dde7
VieiraMidas <guilherme.brasil3@etec.sp.gov.br>
2020-03-09 22:11:34 -0300

    bls

---
+++ app/src/main/java/br/com/etecia/tcc/Login_activity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.tcc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_login);
+    }
+}
