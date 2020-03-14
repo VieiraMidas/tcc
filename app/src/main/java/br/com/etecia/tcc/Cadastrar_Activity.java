6d201d07243fb1f6bbcbce7404ca08a7b39b9e03
VieiraMidas <guilherme.brasil3@etec.sp.gov.br>
2020-03-13 22:21:15 -0300

    Cadastrar Terminando

--- app/src/main/java/br/com/etecia/tcc/Cadastrar_Activity.java
+++ app/src/main/java/br/com/etecia/tcc/Cadastrar_Activity.java
@@ -4,21 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 
-public class Abertura_activity extends AppCompatActivity {
+public class Cadastrar_Activity extends AppCompatActivity {
 
+    Button btnCadastrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_abertura);
+        setContentView(R.layout.cadastrar_layout);
 
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
+        btnCadastrar = findViewById(R.id.btnCadastrar);
+
+        btnCadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
+            public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(),Login_activity.class));
             }
-        },10000);
+        });
     }
 }
