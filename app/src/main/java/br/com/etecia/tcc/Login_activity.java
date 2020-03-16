f1613bb66b81bab2d7d92eef5bcaf60a36d3a676
VieiraMidas <guilherme.brasil3@etec.sp.gov.br>
2020-03-16 20:14:26 -0300

    Interligação entre paginas ok

--- app/src/main/java/br/com/etecia/tcc/Login_activity.java
+++ app/src/main/java/br/com/etecia/tcc/Login_activity.java
@@ -2,13 +2,41 @@ package br.com.etecia.tcc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Login_activity extends AppCompatActivity {
+
+    Button btnEntrar;
+    TextView txtCadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_login);
+
+        btnEntrar = findViewById(R.id.btnEntrar);
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        });
+    }
+
+    public void abrir_cadastrar_activity(View view) {
+
+        txtCadastrar = findViewById(R.id.txtCadastrar);
+
+        txtCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Cadastrar_Activity.class));
+            }
+        });
     }
 }
