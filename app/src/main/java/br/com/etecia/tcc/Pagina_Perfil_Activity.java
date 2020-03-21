0c5349eebe435efa916f9bb2098fe81b01bed6bb
VieiraMidas <guilherme.brasil3@etec.sp.gov.br>
2020-03-20 22:30:03 -0300

    pagina_perfil sendo feita

---
+++ app/src/main/java/br/com/etecia/tcc/Pagina_Perfil_Activity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.tcc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Pagina_Perfil_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_pagina__perfil);
+    }
+}
